fa12efa4bf8834c9dad892eb44bb454ff694cc93
Jose Ramon Marquez <jrms_x@hotmail.com>
2020-06-01 12:01:55 -0500

    Corrección en registrar usuario

--- controllers/register_controller.py
+++ controllers/register_controller.py
@@ -4,11 +4,17 @@ from flask import jsonify, request
 from flask_restful import reqparse, Resource
 import hashlib, uuid
 
+
 class Register(Resource):
     def post(self):
         json = request.get_json()
         username = json["user"]
         password = json["password"]
+
+        foundUser = User.query.filter_by(username=username).first()
+
+        if foundUser is not None:
+            return {"message": "Username {0} already exists".format(username)}, 400
 
         user = User()
         user.username = username
@@ -16,4 +22,4 @@ class Register(Resource):
         user.hash = hashlib.sha512((password + user.salt).encode('utf-8')).hexdigest()
         db.session.add(user)
         db.session.commit()
-        return jsonify(message = "User created")
+        return jsonify(message="User created")
